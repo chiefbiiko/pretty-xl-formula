3111ddb907f2f0fb82d4335f97320e4b37972a15
Noah Schwarz <noah.anabiik.schwarz@gmail.com>
2017-04-20 18:14:34 +0200

    sprintlist name default

---
+++ sprintl.R
@@ -0,0 +1,10 @@
+# sprintl - sprint to a defaulted named list
+
+sprintl <- function(nms, dflt) {
+  stopifnot(is.character(nms),
+            length(nms) > 0L)
+  x <- vector('list', length(nms))
+  if (!is.null(dflt)) x <- lapply(x, function(x) dflt)
+  names(x) <- nms
+  return(x)
+}
