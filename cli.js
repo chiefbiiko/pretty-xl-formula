1f2c86325cb2224c44c63774fa71cc76231a4cad
Noah Schwarz <noah.anabiik.schwarz@gmail.com>
2017-11-14 19:42:38 +0100

    cleanr pipe

--- cli.js
+++ cli.js
@@ -2,25 +2,23 @@
 
 const fs = require('fs')
 const { Readable, Transform } = require('stream')
-const prettify = require('./index')
+const pretty = require('./index')
 
 const line = process.argv.slice(2).join(' ')
-const il = /i(ndent)?l(ength)?=\d/i.test(line)
-  ? parseInt(line.replace(/^.+i(ndent)?l(ength)?=(\d+).*$/i, '$3')) || 2 : 2
+const input = process.argv.slice(2).filter(a => !/il=\d/i.test(a))[0] || ''
+const il = /il=\d/i.test(line)
+  ? parseInt(line.replace(/^.*il=(\d+).*$/i, '$1')) || 2 : 2
 
 const pxlf = new Transform({
   transform (chunk, _, next) {
-    this.push(prettify(chunk.toString(), {
-      indentType: ' ',
-      indentLength: il
-    }))
+    this.push(pretty(chunk.toString(), { indentType: ' ', indentLength: il }))
     next()
   }
 })
 
-const makeReadable = x => {
+const makeReadable = stryng => {
   const r = new Readable()
-  r.push(x.toString())
+  r.push(stryng)
   r.push(null)
   return r
 }
@@ -33,12 +31,12 @@ const help = 'usage:\n    pxlf file\n  or\n' + '    pxlf < file\n  or\n' +
              '  -il: indent length, defaults to -il=2\n\n' +
              '  when using this cli indent type always is " "\n'
 
-if (process.argv.slice(2).some(a => /--?h(elp)?/.test(a))) {
+if (/--?h(elp)?/i.test(line)) {
   makeReadable(help).pipe(process.stdout)
-} else if (fs.existsSync(process.argv[2])) {
-  fs.createReadStream(process.argv[2]).pipe(pxlf).pipe(process.stdout)
-} else if (process.argv[2] && !/i(ndent)?l(ength)?=/i.test(process.argv[2])) {
-  makeReadable(process.argv[2]).pipe(pxlf).pipe(process.stdout)
+} else if (fs.existsSync(input)) {
+  fs.createReadStream(input).pipe(pxlf).pipe(process.stdout)
+} else if (input) {
+  makeReadable(input).pipe(pxlf).pipe(process.stdout)
 } else {
   process.stdin.pipe(pxlf).pipe(process.stdout)
   global.setTimeout(() => process.exit(0), 250)
